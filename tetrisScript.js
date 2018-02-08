92c263868785c3e8f3492da37cbfcb2b3bcb2541
Mark Fitzpatrick <markfitz815@gmail.com>
2018-02-08 17:58:25 +0000

    board now refreshes every 400ms to new values

--- tetrisScript.js
+++ tetrisScript.js
@@ -1,4 +1,4 @@
-function creElT(type, cls, apnd, inHL){
+function creElT(type, cls, apnd, inHL, id){
     let newEl = document.createElement(type);
     if(typeof(cls) == 'string'){
         newEl.classList.add(cls);
@@ -8,6 +8,7 @@ function creElT(type, cls, apnd, inHL){
         }
     }
     if(inHL){newEl.innerHTML=inHL}
+    if(id){newEl.id = id};
     apnd.appendChild(newEl);
 }
 
@@ -18,16 +19,37 @@ let tetrisBoard = {
         let yRun = 0;
         for( let i = 0; i < 180; i++){
             let newTileObj = {}
+            newTileObj.classes = ['tile'];
             newTileObj.x = xRun;
             newTileObj.y = yRun;
+            if(xRun == 0){newTileObj.classes.push('firstXTile')}
             xRun++;
-            if(xRun == 10){xRun = 0; yRun++}
+            if(xRun == 10){xRun = 0; yRun++; };
+            
+            newTileObj.empty = true;
+            newTileObj.color = '';
             this.tileArr.push(newTileObj);
         }
+        
+    },
+    drawMap: function(arr){
+        for(let i = 0; i < arr.length; i++){
+            creElT('div', arr[i].classes, document.getElementsByClassName('map')[0]);
+        }
+    },
+    intervalCall: function(arr){
+        document.getElementsByClassName('map')[0].innerHTML = '';
+        this.drawMap(arr);
+        setTimeout(function(){tetrisBoard.intervalCall(tetrisBoard.tileArr);}, 400);
     }
 };
 
 (function initApp(){
-    creElT('div', ['cat','dog'], document.body, 'cat' )
+    creElT('div', 'app', document.body, '', 'app');
+    creElT('div', 'map', document.getElementById('app'));
     tetrisBoard.tileInit();
+    tetrisBoard.drawMap(tetrisBoard.tileArr);
+    setTimeout(function(){
+        tetrisBoard.intervalCall(tetrisBoard.tileArr);
+    }, 400)
 })()
