5defbbf39ad937956f1b34bb25578b4f4ebe8dc6
Mark Fitzpatrick <markfitz815@gmail.com>
2018-02-12 14:56:41 +0000

    Created let which holds current piece and created function which randomly assigns a piece and direction to it

--- tetrisScript.js
+++ tetrisScript.js
@@ -1,3 +1,4 @@
+//function for creating elements
 function creElT(type, cls, apnd, inHL, id){
     let newEl = document.createElement(type);
     if(typeof(cls) == 'string'){
@@ -11,18 +12,18 @@ function creElT(type, cls, apnd, inHL, id){
     if(id){newEl.id = id};
     apnd.appendChild(newEl);
 }
-
+//Object holds all data
 let tetrisBoard = {
     tileArr : [],
     pieces: [],
+    currPiece: {name : '', map: '', direction: ''},
     makePieces: function(piece){
         let newOb = {}
-        for(let h = 0; h < piece.length; h++){
+        for(let h = 0; h < piece.length-1; h++){
             let newPiece = [];
             for(let i = 0; i < 16; i++){
                 let sameNum = false;
                 for(let j = 0; j < piece[h].length; j++){
-                    console.log('sap')
                     if(piece[h][j] == i){sameNum = true}
                 }
                 if(sameNum){newPiece.push(1)}
@@ -33,24 +34,40 @@ let tetrisBoard = {
             if(h == 2){newOb.s = newPiece};
             if(h == 3){newOb.w = newPiece};
         }
+        newOb.name = piece[4];
         this.pieces.push(newOb);
     },
+    //Makes pieces basic information
     makePiecesBlueprint: function(){
         //Line
-        this.makePieces([[2,6,10,14], [8,9,10,11], [1,5,9,13], [4,5,6,7]]);
+        this.makePieces([[2,6,10,14], [8,9,10,11], [1,5,9,13], [4,5,6,7], 'Line']);
         //Square
-        this.makePieces([[5,6,9,10], [5,6,9,10], [5,6,9,10], [5,6,9,10]]);
+        this.makePieces([[5,6,9,10], [5,6,9,10], [5,6,9,10], [5,6,9,10], 'Square']);
         //L-Shape
-        this.makePieces([[1,5,9,10], [2,4,5,6], [0,1,5,9], [4,5,6,8]]);
+        this.makePieces([[1,5,9,10], [2,4,5,6], [0,1,5,9], [4,5,6,8], 'L-Shape']);
         //J-Shape
-        this.makePieces([[1,5,9,10], [0,4,5,6], [1,2,5,9], [4,5,6,10]]);
+        this.makePieces([[1,5,9,10], [0,4,5,6], [1,2,5,9], [4,5,6,10], 'J-Shape']);
         //Tee
-        this.makePieces([[4,5,6,9], [1,4,5,9], [1,5,6,9], [1,4,5,6]]);
+        this.makePieces([[4,5,6,9], [1,4,5,9], [1,5,6,9], [1,4,5,6], 'Tee']);
         //Z-Shape
-        this.makePieces([[2,5,6,9], [4,5,9,10], [1,4,5,8], [0,1,5,6]]);
+        this.makePieces([[2,5,6,9], [4,5,9,10], [1,4,5,8], [0,1,5,6], 'Z-Shape']);
         //S-Shape
-        this.makePieces([[1,5,6,10], [5,6,8,9], [0,4,5,9], [1,2,4,5]]);
+        this.makePieces([[1,5,6,10], [5,6,8,9], [0,4,5,9], [1,2,4,5], 'S-Shape']);
     },
+    //Picks current Piece
+    selectPiece: function(arr){
+        
+        let spRand = Math.floor(Math.random()*arr.length);
+        let spRand2 = Math.floor(Math.random()*4);
+        if(spRand2 == 0){this.currPiece.map = arr[spRand].n, this.currPiece.direction = 'n'}
+        if(spRand2 == 1){this.currPiece.map = arr[spRand].e, this.currPiece.direction = 'e'}
+        if(spRand2 == 2){this.currPiece.map = arr[spRand].s, this.currPiece.direction = 's'}
+        if(spRand2 == 3){this.currPiece.map = arr[spRand].w, this.currPiece.direction = 'w'}
+        this.currPiece.name = arr[spRand].name;
+        
+        console.log(this.currPiece)
+    },
+    //Creates tile positions
     tileInit: function(){
         let xRun = 0;
         let yRun = 0;
@@ -69,6 +86,7 @@ let tetrisBoard = {
         }
         
     },
+    //Draws map to screen
     drawMap: function(arr){
         for(let i = 0; i < arr.length; i++){
             creElT('div', arr[i].classes, document.getElementsByClassName('map')[0]);
@@ -81,12 +99,14 @@ let tetrisBoard = {
     }
 };
 
+//Initialize Application
 (function initApp(){
     creElT('div', 'app', document.body, '', 'app');
     creElT('div', 'map', document.getElementById('app'));
     tetrisBoard.makePiecesBlueprint();
     tetrisBoard.tileInit();
     tetrisBoard.drawMap(tetrisBoard.tileArr);
+    tetrisBoard.selectPiece(tetrisBoard.pieces)
     setTimeout(function(){
         tetrisBoard.intervalCall(tetrisBoard.tileArr);
     }, 400)
